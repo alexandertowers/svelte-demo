5138b0e1e6e31288e98124b9595d2010f2fdf50c
Alex <alexanderjtowers@gmail.com>
2022-07-19 16:25:38 +1200

    Added menu file and pages to match these menu items

---
+++ public/components/menu.svelte
@@ -0,0 +1,14 @@
+<script>
+    let menu_items = [
+        'Home',
+        'Customers',
+        'AUA',
+        'Products',
+        'Geography',
+        'Experience'
+    ]
+</script>
+
+{#each menu_items as menu}
+    <li><a href={'/'+menu}>{menu}</a></li>
+{/each}
