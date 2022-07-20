010ff515008352badf0d3c21c39ab5593fd5e7e6
Alex <alexanderjtowers@gmail.com>
2022-07-20 16:57:25 +1200

    Dashboard changes

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -2,41 +2,38 @@
     import Stat from  "../components/stat.svelte";
     import "@carbon/styles/css/styles.css";
     import "@carbon/charts/styles.css";
-    import LineGraph from '../components/charts/line.svelte';
     let quickStats = [
-        'Number of Customers',
-        'Assets under Administration',
-        'Attrition',
-        'Open Issues',
-        'Open Requests'
+        {'title':'Number of Customers', 'value':'12345', 'desc':'↗︎ 400 (22%)'},
+        {'title':'Assets under Administration', 'value':'$1,543,299.00', 'desc':''},
+        {'title':'Attrition', 'value':'2%', 'desc':'Jan 1st - Feb 1st'},
+        {'title':'Open Issues', 'value':'12', 'desc':'↘︎ 9 (14%)'},
+        {'title':'Open Requests', 'value':'5', 'desc':'↘︎ 1 (10%)'}
     ];
-    let show = false;
-    let data = [
-		{age: 24, temp: 37.2},
-		{age: 24.1, temp: 37.1},
-		{age: 24.2, temp: 36.8},
-		{age: 24.3, temp: 37.0},
-		{age: 24.4, temp: 36.7},
-		{age: 24.5, temp: 37.2},
-		{age: 24.6, temp: 36.5},
-		{age: 24.7, temp: 36.9},
-	];
-    function showGraph(){
-        show = !show;
-    }
 </script>
 
-<div class="flex flex-col lg:flex-row m-5">
-    {#each quickStats as stat}
-        <div on:click={showGraph} class="grid flex-grow h-32 card bg-base-300 rounded-box">
-            <Stat title={stat} value={Math.round(Math.random()*100, 2)}/>
-        </div>
-        {#if quickStats.indexOf(stat) != quickStats.length - 1}
-            <div class="divider lg:divider-horizontal"></div>
-        {/if}
-    {/each}
-    
+<div class="flex flex-col justify-center lg:flex-row m-5">
+    <div class="stats stats-vertical w-full lg:stats-horizontal shadow">
+        {#each quickStats as stat}
+            <Stat title={stat.title} value={stat.value} desc={stat.desc} />
+        {/each}
+      </div>
 </div>
-<div class="flex flex-col lg:flex-row m-5">
-    <LineGraph {data} {show} />
+<div class="flex flex-col flex-row lg:flex-row">
+    <div class="card lg:w-2/3 min-h-max bg-base-100 shadow-lg m-5 w-full">
+        <div class="card-body">
+          <h2 class="card-title">Health Metrics</h2>
+          <p>How are you performing?</p>
+        </div>
+    </div>
+    <div class="card lg:w-1/3 min-h-max bg-base-100 shadow-lg m-5 w-full">
+        <div class="card-body">
+          <h2 class="card-title">Recommendations</h2>
+          <p>Top 5 Actions for You</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">See more</button>
+          </div>
+        </div>
+    </div>
+
+      
 </div>
