17164121960920e66462563f7fc0007383edac51
Alex <alexanderjtowers@gmail.com>
2022-07-20 13:29:26 +1200

    Carbon charts added - Plus some fiddling

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,5 +1,8 @@
 <script>
-    import Stat from  "../../components/stat.svelte";
+    import Stat from  "../components/stat.svelte";
+    import "@carbon/styles/css/styles.css";
+    import "@carbon/charts/styles.css";
+    import LineGraph from '../components/charts/line.svelte';
     let quickStats = [
         'Number of Customers',
         'Assets under Administration',
@@ -7,15 +10,33 @@
         'Open Issues',
         'Open Requests'
     ];
+    let show = false;
+    let data = [
+		{age: 24, temp: 37.2},
+		{age: 24.1, temp: 37.1},
+		{age: 24.2, temp: 36.8},
+		{age: 24.3, temp: 37.0},
+		{age: 24.4, temp: 36.7},
+		{age: 24.5, temp: 37.2},
+		{age: 24.6, temp: 36.5},
+		{age: 24.7, temp: 36.9},
+	];
+    function showGraph(){
+        show = !show;
+    }
 </script>
 
 <div class="flex flex-col lg:flex-row m-5">
     {#each quickStats as stat}
-        <div class="grid flex-grow h-32 card bg-base-300 rounded-box">
+        <div on:click={showGraph} class="grid flex-grow h-32 card bg-base-300 rounded-box">
             <Stat title={stat} value={Math.round(Math.random()*100, 2)}/>
         </div>
         {#if quickStats.indexOf(stat) != quickStats.length - 1}
             <div class="divider lg:divider-horizontal"></div>
         {/if}
     {/each}
+    
+</div>
+<div class="flex flex-col lg:flex-row m-5">
+    <LineGraph {data} {show} />
 </div>
--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,12 @@ import adapter from '@sveltejs/adapter-static';
 import preprocess from "svelte-preprocess";
 
 const config = {
+  kit: {
+    adapter: adapter(),
+    prerender: {
+      default: true
+    },
+  },
   preprocess: [
     preprocess({
       postcss: true,
@@ -10,19 +16,4 @@ const config = {
   ],
 }
 
-export default {
-  kit: {
-    adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
-      pages: 'build',
-      assets: 'build',
-      fallback: null,
-      precompress: false
-    }),
-    prerender: {
-      // This can be false if you're using a fallback (i.e. SPA mode)
-      default: true
-    }
-  }
-};
+export default config;
